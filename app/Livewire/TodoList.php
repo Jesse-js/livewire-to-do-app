d609593507f99ce177f67ecbdb3c62526d914ae2
Jesse-js <jesse-jjs@hotmail.com>
2024-03-07 00:19:02 -0300

    feat: create method

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -2,12 +2,26 @@
 
 namespace App\Livewire;
 
+use App\Models\Todo;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class TodoList extends Component
 {
+    #[Validate('required|min:3|max:150')]
     public string $name;
-    
+
+    public function create()
+    {
+        $validated = $this->validate();
+        Todo::create([
+            'name' => $validated['name']
+        ]);
+
+        $this->reset();
+
+        request()->session()->flash('success', 'The todo was created!');
+    }
     public function render()
     {
         return view('livewire.todo-list');
