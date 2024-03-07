d2a627eae3c4f09966e6423f7568d4d33b5d9604
Jesse-js <jesse-jjs@hotmail.com>
2024-03-07 12:24:51 -0300

    feat: add update name

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -16,6 +16,10 @@ class TodoList extends Component
     public string $name = '';
 
     public string $search = '';
+    public int $todoId = 0;
+
+    #[Validate('required|min:3|max:50')]
+    public string $editName = '';
 
     public function create(): void
     {
@@ -40,6 +44,27 @@ class TodoList extends Component
         $todo = Todo::findOrFail($todoId);
         $todo->completed = !$todo->completed;
         $todo->save();
+    }
+
+    public function edit(int $todoId): void 
+    {
+        $this->todoId = $todoId;
+        $this->editName = Todo::find($todoId)->name;   
+    }
+
+    public function cancelEdit() 
+    {
+        $this->reset('todoId', 'editName');
+    }
+
+    public function update() 
+    {
+        $validated = $this->validateOnly('editName');
+        Todo::find($this->todoId)->update([
+            'name' => $validated['editName']
+        ]);
+
+        $this->cancelEdit();
     }
 
     public function render(): View
